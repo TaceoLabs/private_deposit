ed765640a013a9d45625044dce1ee443deb452a4
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2025-12-05 15:54:53 +0100

    fix: Fix a testcase for ERC contract

--- contracts/script/test/withdraw_erc.s.sol
+++ contracts/script/test/withdraw_erc.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
-import {ConfidentialTokenERC} from "../../src/conf_token.sol";
+import {ConfidentialTokenERC} from "../../src/conf_token_erc.sol";
 
 contract ConfidentialTokenERCScript is Script {
     ConfidentialTokenERC public conf_token;
